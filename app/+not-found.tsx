742b38cfca60f391308c2f3c17456abae36e1deb
Michael Real <@>
2024-12-16 22:35:57 +0100

    LFG one more time

---
+++ app/+not-found.tsx
@@ -0,0 +1,33 @@
+import { Link, Stack } from "expo-router";
+
+import { View, StyleSheet } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops! Not Found" }} />
+      <View style={styles.container}>
+        <Link href="./(tabs)" style={styles.button}>
+          Go back to Home screen!
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    color: "#fff",
+    textDecorationLine: "underline",
+  },
+});
